1591c407882b995ec100c26e85200158197b3934
lemon666666 <1848999832@qq.com>
2020-11-22 23:33:16 +0800

    11/22

---
+++ src/components/ColumnList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export interface columnProps {
+  id: Number,
+  title: String,
+  avatar: String,
+  desc: String
+}
+export default defineComponent({
+  name: 'ColumnList',
+  props: {
+    list: {
+      type: Array,
+      required: true
+    }
+  }
+})
+</script>
