53eed505cdcea7f9c6576808931722f32ef0d412
lemon666666 <1848999832@qq.com>
2020-11-23 01:19:25 +0800

    globalheader

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,33 @@
+<template>
+    <nav class="navbar navbar-dark bg-primary mb-4 justify-content-between">
+      <a class="navbar-brand" href="#">无敌专栏</a>
+      <ul v-if="!user.isLogin" class="list-inline mb-0">
+        <li class="list-inline-item"><a href="#" class="btn btn-outline-light my-2">登录</a></li>
+        <li class="list-inline-item"><a href="#" class="btn btn-outline-light my-2">注册</a></li>
+      </ul>
+      <ul v-else class="list-inline mb-0">
+        <li class="list-inline-item"><a href="#" class="btn btn-outline-light my-2">你好 {{user.name}}</a></li>
+      </ul>
+    </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+export interface UserProps {
+    isLogin: boolean;
+    name?: string;
+    id?: number;
+}
+export default defineComponent({
+  name: 'GlobalHeader',
+  props: {
+    user: {
+      type: Object as PropType<UserProps>,
+      required: true
+    }
+  }
+})
+</script>
+
+<style lang="stylus" scoped>
+</style>
